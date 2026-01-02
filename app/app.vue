7303ec59cb1df70e5662e8091389a0b0bfbcb1ca
Sheraz Jutt <sherazarshad419@gmail.com>
2026-01-02 20:21:12 +0500

    Update configuration files and styles for improved application structure and performance. Adjust .prettierrc settings, enhance nuxt.config.ts with Tailwind CSS integration, and refine main.css for better styling. Update package.json dependencies and pnpm workspace settings to include new packages and streamline builds.

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,5 @@
 <template>
-	<UApp :toaster="{ position: 'bottom-right' }">
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
-	</UApp>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,39 +1,44 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-11-01",
-	devtools: { enabled: true },
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
 
-	app: {
-		// pageTransition: { name: 'page', mode: 'out-in' },
-		rootId: "__velocepress",
-		rootTag: "main",
-		head: {
-			title: "Wodwes Page Builder",
-			meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
-			link: [
-				// { rel: 'icon', type: 'image/svg', href: '/favicon.png' },
-				{ rel: "preconnect", href: "https://fonts.googleapis.com" },
-				{ rel: "preconnect", href: "https://fonts.gstatic.com" },
-				{ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" },
-			],
-			// script: [{ src: '', async: true }],
-		},
-	},
+  app: {
+    // pageTransition: { name: 'page', mode: 'out-in' },
+    rootId: "__velocepress",
+    rootTag: "main",
+    head: {
+      title: "Wodwes Page Builder",
+      meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
+      link: [
+        // { rel: 'icon', type: 'image/svg', href: '/favicon.png' },
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" },
+      ],
+      // script: [{ src: '', async: true }],
+    },
+  },
 
-	modules: ["@nuxt/ui", "@vueuse/nuxt"],
-	css: ["~/assets/css/main.css"],
-	ui: {
-		colorMode: false,
-	},
+  modules: ["@nuxt/ui", "@vueuse/nuxt"],
+  css: ["./app/assets/css/main.css"],
+  ui: {
+    colorMode: false,
+  },
 
-	alias: {
-		"@": "~/",
-		"@images": "~/assets/images",
-	},
+  alias: {
+    "@": "~/",
+    "@images": "~/assets/images",
+  },
 
-	runtimeConfig: {
-		public: {
-			pocketbaseUrl: "https://pocketbase-ptgwn.sevalla.app/",
-		},
-	},
+  runtimeConfig: {
+    public: {
+      pocketbaseUrl: "https://pocketbase-ptgwn.sevalla.app/",
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
